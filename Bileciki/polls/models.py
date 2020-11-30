8188c4dc69608794bdecc37339cf21472c0340f2
mimichal1998 <56155731+mimichal1998@users.noreply.github.com>
2020-11-30 15:20:42 +0100

    Django

--- Bileciki/polls/models.py
+++ Bileciki/polls/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Klient(models.Model):
+    idKlient = models.IntegerField(primary_key=True)
+    Imie = models.CharField(max_length=50)
+    Nazwisko = models.CharField(max_length=50)
+    Wiek = models.IntegerField()
+    NrTel = models.IntegerField()
+    Email = models.CharField(max_length=200)
+    RodzajUlgi = models.CharField(max_length=100)
+
+
+class Przystanek(models.Model):
+    idPrzysanek = models.IntegerField(primary_key=True)
+    Nazwa = models.CharField(max_length=100)
+
+class Trasa(models.Model):
+    idTrasa = models.IntegerField(primary_key=True)
+    czas = models.DateTimeField()
+    idPrzystanek = models.ForeignKey(Przystanek, on_delete=models.CASCADE)
+
+
+class Kurs(models.Model):
+    idKurs = models.AutoField()
+    Przewoniznik = models.CharField(max_length=45)
+    ilosc_miejsc = models.BigIntegerField()
+    Cena = models.FloatField()
+    idTrasa = models.ForeignKey(Trasa, on_delete=models.CASCADE)
+
+
+class KupioneBilety(models.Model):
+    idBilet = models.IntegerField(primary_key=True)
+    idKlient = models.ForeignKey(Klient, on_delete=models.CASCADE)
+    idKurs = models.ForeignKey(Kurs, on_delete=models.CASCADE)
+
+
+
